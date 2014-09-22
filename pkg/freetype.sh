fb6f180fc0a33de8c7cd88e8d46a29154c2095e1
Lukasz Szpakowski <luckboy@vp.pl>
2014-09-22 11:02:19 +0200

    Fixed pkg/freetype.sh.

--- pkg/freetype.sh
+++ pkg/freetype.sh
@@ -3,3 +3,12 @@ CC="$MUSL_GCC" CFLAGS="$PKG_CFLAGS" LDFLAGS="$PKG_LDFLAGS" LIBS="$PKG_LIBS" STRI
 LIBPNG_CFLAGS="-I$ROOT_DIR/bin/$ARCH/libpng_dev/usr/include" \
 LIBPNG_LIBS="-L$ROOT_DIR/bin/$ARCH/libpng/usr/lib -L$ROOT_DIR/bin/$ARCH/libpng_dev/usr/lib -lpng" \
 ./configure --host="$TARGET" --prefix=/usr --sysconfdir=/etc --localstatedir=/var && make install DESTDIR="$ROOT_DIR/bin/$ARCH/$PKG_NAME"
+STATUS=$?
+if [ $STATUS = 0 ]; then
+	[ "`ls "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/bin"`" = "" ] && rmdir "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/bin"
+	[ "`ls "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/share/aclocal"`" = "" ] && rmdir "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/share/aclocal"
+	[ "`ls "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/share/man/man1"`" = "" ] && rmdir "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/share/man/man1"
+	[ "`ls "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/share/man"`" = "" ] && rmdir "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/share/man"
+	[ "`ls "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/share"`" = "" ] && rmdir "$ROOT_DIR/bin/$ARCH/$PKG_NAME/usr/share"
+fi
+[ $STATUS = 0 ]
