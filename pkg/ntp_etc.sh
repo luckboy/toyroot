4e454de64deb069beb2545143e182512ef61ca91
Lukasz Szpakowski <luckboy@vp.pl>
2014-10-21 15:22:09 +0200

    Added ntp.

---
+++ pkg/ntp_etc.sh
@@ -0,0 +1,6 @@
+mkdir -p "$PKG_ROOT_DIR/etc/rc.d"
+chmod 755 "$PKG_ROOT_DIR/etc/init.d/ntpd"
+chmod 755 "$PKG_ROOT_DIR/etc/init.d/ntpdate"
+[ -e "$PKG_ROOT_DIR/etc/init.d/ntp" ] || ln -s ntpdate "$PKG_ROOT_DIR/etc/init.d/ntp"
+ln -s ../init.d/ntpdate "$PKG_ROOT_DIR/etc/rc.d/S25ntp"
+ln -s ../init.d/ntpdate "$PKG_ROOT_DIR/etc/rc.d/K25ntp"
